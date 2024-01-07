3d0e905cbbd7ec98245519f3a016ae82f06d0b91
Mahmoud Abdelaty <126201317+Mahmoud-Abdelaty@users.noreply.github.com>
2024-01-07 20:23:46 +0200

    first commit

--- lib/widgets/categories_list_view_builder.dart
+++ lib/widgets/categories_list_view_builder.dart
@@ -1,23 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:news_app/constant/variables.dart';
 import 'package:news_app/widgets/categories_list_view.dart';
-import 'package:news_app/widgets/loading_category_card.dart';
 
 class CategoriesListviewBuilder extends StatelessWidget {
   const CategoriesListviewBuilder({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
+    return const SizedBox(
       height: 100,
-      child: isLoaded
-          ? const CategoriesListview()
-          : ListView.builder(
-              physics: const BouncingScrollPhysics(),
-              itemCount: 5,
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (context, index) => const LoadingCategoryCard(),
-            ),
+      child: CategoriesListview(),
     );
   }
 }
